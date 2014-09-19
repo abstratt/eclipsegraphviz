035cac7a5a45c42afe7e6c3f8ea67e7103e1c8eb
Rafael Chaves <rafael@abstratt.com>
2014-09-19 10:42:47 -0300

    renamed method, added another version

--- plugins/com.abstratt.pluginutils/src/com/abstratt/pluginutils/ConfigUtils.java
+++ plugins/com.abstratt.pluginutils/src/com/abstratt/pluginutils/ConfigUtils.java
@@ -1,7 +1,10 @@
 package com.abstratt.pluginutils;
 
 public class ConfigUtils {
-    public static String getSetting(String name, String defaultValue) {
+    public static String get(String name) {
+        return get(name, null);
+    }
+    public static String get(String name, String defaultValue) {
         String asProperty = System.getProperty(name);
         if (asProperty != null)
             return asProperty;

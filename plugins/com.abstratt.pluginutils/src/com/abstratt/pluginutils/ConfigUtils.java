d27e8e0751ab0b843c060214eb6f7ef4cbab77bb
Rafael Chaves <rafael@abstratt.com>
2014-09-19 10:12:27 -0300

    config helper

---
+++ plugins/com.abstratt.pluginutils/src/com/abstratt/pluginutils/ConfigUtils.java
@@ -0,0 +1,13 @@
+package com.abstratt.pluginutils;
+
+public class ConfigUtils {
+    public static String getSetting(String name, String defaultValue) {
+        String asProperty = System.getProperty(name);
+        if (asProperty != null)
+            return asProperty;
+        String asEnvVar = System.getenv(name);
+        if (asEnvVar != null)
+            return asEnvVar;
+        return defaultValue;
+    }
+}

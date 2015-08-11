5cde6611de82478e92a5be6e017db3c0e7b76ad1
Rafael Chaves <rafael@abstratt.com>
2015-08-11 15:02:41 -0300

    added clarification to IProviderDescription

--- plugins/com.abstratt.content/src/com/abstratt/content/IContentProviderRegistry.java
+++ plugins/com.abstratt.content/src/com/abstratt/content/IContentProviderRegistry.java
@@ -13,6 +13,13 @@ public interface IContentProviderRegistry {
 
 		public IContentProvider getProvider();
 
+		/**
+		 * Converts the given source object (using one of the contributed readers) 
+		 * to the format expected by the content provider. 
+		 * 
+		 * @param source object to convert
+		 * @return the converted contents that are consumable by the content provider
+		 */
 		public Object read(Object source);
 	}
 
